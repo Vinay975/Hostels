76a0b03c9d433c77414acc10dc39753724c175ec
Vinay975 <22a91a61c0@aec.edu.in>
2024-12-24 11:33:44 +0530

    change

--- frontend/hostals/src/components/Navbar.jsx
+++ frontend/hostals/src/components/Navbar.jsx
@@ -1,15 +1,15 @@
-import react from "react"
-import {Outlet,Link} from "react-router-dom";
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 const Navbar = () => {
-    return(
+    return (
         <>
-        <nav className="nav">
-      <div className="logo"><a href="/">Hostals</a></div>
-    <Link to="/" className="Link"><div className="navdiv a">Add Your Place</div></Link>
-    <Link to="/aboutus" className="Link"><div className="navdiv b">location</div></Link>
-    <Link to="/login" className="Link"><div className="navdiv c">profile</div></Link>    
-    </nav>
-    <Outlet/>
+            <nav className="nav">
+                <div className="logo"><a href="/">Hostels</a></div>
+                <Link to="/" className="Link"><div className="navdiv a">Add Your Place</div></Link>
+                <Link to="/aboutus" className="Link"><div className="navdiv b">location</div></Link>
+                <Link to="/login" className="Link"><div className="navdiv c">profile</div></Link>
+            </nav>
+            <Outlet />
         </>
     )
 };

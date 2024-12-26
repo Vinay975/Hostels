a08213fdaff2a7e13073624ae54c9563b60d4b4e
venugopal <119839956+venugopal8005@users.noreply.github.com>
2024-12-26 19:44:58 +0530

    profile

--- frontend/hostals/src/components/Navbar.jsx
+++ frontend/hostals/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/navbar.css";
 import { Outlet, Link } from "react-router-dom";
 import { IoMenuSharp } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import Gaintbar from "./Gaintbar";
+import Userin from "./Userin";
+
 const Navbar = () => {
+    const [clicked , setclicked] = useState(false);
     const profile_action = ()=>{
-        
+        setclicked(!clicked);
     }
     return (
         <>
@@ -14,7 +17,7 @@ const Navbar = () => {
                 <div className="logo"><a href="/">Logo</a></div>
                 <Link to="/" className="Link"><div className="location">Location</div></Link>
     <Link to="/ownerdashboard" className="Link"><div className="navdiv b">Add Your Place</div></Link>
-    <div className="c" onClick={}><IoMenuSharp/><CgProfile /></div>
+    <div className="c" onClick={profile_action}><IoMenuSharp/><CgProfile /></div>
 
             </nav>
             <Gaintbar/>
---
+++ frontend/hostals/src/components/Userin.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "../CSS/userin.css"
+const Userin = (props) => {
+  return (
+    <div  className="options">
+      
+    </div>
+  )
+};
+
+export default Userin;

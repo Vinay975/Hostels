da99fe80104e36e254f93d4abe6e50080583274a
venugopal <119839956+venugopal8005@users.noreply.github.com>
2025-01-02 20:42:11 +0530

    idkkkkk

--- frontend/hostals/src/components/filter_results/FilterResults.jsx
+++ frontend/hostals/src/components/filter_results/FilterResults.jsx
@@ -5,16 +5,16 @@ const FilterResults = (props) => {
   return (
     <div className="filterresults">
         
-         {/* <div className="placecards">
+         <div className="placecards">
             <div className="placecard">
-            <div className="image"><img className="image" src="https://a0.muscache.com/im/pictures/d5962b48-8d1d-4dd0-bba6-101137cbe973.jpg?im_w=720&im_format=avif" alt="" /></div>
-            <div className="details">
-                <h2>Hostel Name</h2>
+            <div><img className="image" src="https://a0.muscache.com/im/pictures/d5962b48-8d1d-4dd0-bba6-101137cbe973.jpg?im_w=720&im_format=avif" alt="" /></div>
+            <div className="info">
+                <h2 className="">Hostel Name</h2>
                 <p>Location</p>
                 <p>Price</p>
             </div>
             </div>
-         </div> */}
+         </div>
         
     </div>
   )

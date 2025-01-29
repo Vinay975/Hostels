c5b72389eada9d512706e6627b33a078adb7a2d4
Vinay975 <22a91a61c0@aec.edu.in>
2025-01-29 22:53:39 +0530

    hostelDataone

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const authRoutes = require("./Routes/userDataAuth");
+
 
 const app = express();
 const PORT = 5000;
@@ -12,10 +12,9 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads")); 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
-app.use("/api/auth", authRoutes);
 
 // MongoDB Connection
 mongoose
@@ -27,3 +26,11 @@ mongoose
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err.message));
+
+// Routes
+const authRoutes = require("./Routes/userDataAuth");
+app.use("/api/auth", authRoutes);
+
+//hostel-1-routes
+const hosteloneRoutes = require("./Routes/hostel-1-routes")
+app.use("/api", hosteloneRoutes);
